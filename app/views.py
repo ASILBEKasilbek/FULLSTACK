59287a8fe4bc369073cb0f5ec44834eb12c98fed
Asilbek <asilbek.sadullayev000@example.com>
2025-06-16 08:25:41 +0500

    allowed

--- app/views.py
+++ app/views.py
@@ -10,7 +10,6 @@ def home_view(request):
     solved = SupportRequest.objects.filter(status='solved').order_by('-solved_at')[:10]
     return render(request, 'main.html', {'solved': solved})
 
-
 @login_required
 def services_view(request):
     form = SupportRequestForm()
@@ -32,7 +31,6 @@ def services_view(request):
         'pending': pending,
     })
 
-# Profil bo‘limi
 @login_required
 def profile_view(request):
     user_requests = SupportRequest.objects.filter(user=request.user)
@@ -43,7 +41,6 @@ def profile_view(request):
         'pending_requests': pending_requests,
     })
 
-# Ro'yxatdan o'tish
 def register_view(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
@@ -54,7 +51,6 @@ def register_view(request):
         form = RegisterForm()
     return render(request, 'register/register.html', {'form': form})
 
-# Tizimga kirish
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
@@ -66,22 +62,16 @@ def login_view(request):
         form = AuthenticationForm()
     return render(request, 'register/login.html', {'form': form})
 
-# Tizimdan chiqish
 def logout_view(request):
     logout(request)
     return redirect('login')
 
-# Asosiy sahifa (home)
 def main(request):
     return render(request, 'main.html')
 
-# Contact sahifa
 def contact(request):
     return render(request, 'contact.html')
 
-# Yordamlar bo‘limi (FAQ)
 def yordamlar(request):
     faqs = FAQ.objects.all()
     return render(request, 'yordamlar.html', {'faqs': faqs})
-
-# Home sahifasida faqat hal qilingan muammolarni ko‘rsatish
